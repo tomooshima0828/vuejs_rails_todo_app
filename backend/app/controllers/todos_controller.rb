d9e80b5178d37437140262c71a913b7f3249957d
tomooshima0828 <tomo.oshima0828@gmail.com>
2024-03-13 18:24:13 +0900

    createアクションを追加

--- backend/app/controllers/todos_controller.rb
+++ backend/app/controllers/todos_controller.rb
@@ -1,6 +1,21 @@
 class TodosController < ApplicationController
   def index
-    @todos = Todo.all
+    @todos = Todo.order(created_at: :desc)
     render json: @todos
+  end
+
+  def create
+    @todo = Todo.new(todo_params)
+    if @todo.save
+      render json: @todo, status: :created
+    else
+      render json: @todo.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def todo_params
+    params.require(:todo).permit(:title, :completed)
   end
 end
--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="app">
     <h1>Todoリスト</h1>
+    <div>
+      <input v-model="newTodoTitle" placeholder="新しいTodoを追加">
+      <button @click="addTodo">登録</button>
+    </div>
     <ul>
       <li v-for="todo in todos" :key="todo.id">
         {{ todo.title }} - {{ todo.completed ? '完了' : '未完了' }}
@@ -24,8 +28,25 @@ export default {
   },
   methods: {
     async fetchTodos() {
-      const response = await axios.get('http://localhost:3000/todos')
-      this.todos = response.data;
+      try {
+        const response = await axios.get('http://localhost:3000/todos');
+        this.todos = response.data;
+      } catch (error) {
+        console.error('Todoリストの取得に失敗しました:', error);
+      }
+      
+    },
+    async addTodo() {
+      try {
+        await axios.post('http://localhost:3000/todos', {
+          title: this.newTodoTitle,
+          completed: false
+        });
+      this.newTodoTitle = ''; // 入力フィールドをクリア
+      this.fetchTodos(); // Todoリストを再取得
+      } catch (error) {
+        console.error('Todoの追加に失敗しました:', error);
+      }
     }
   }
 };
