4b651155ebf71079d8cc8af0b64afa834c220030
tomooshima0828 <tomo.oshima0828@gmail.com>
2024-03-13 17:46:02 +0900

    CORS設定とtodoリストの表示ができたためコミット

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,12 +1,33 @@
 <template>
   <div id="app">
-    <h1>Hello, Vue.js!</h1>
+    <h1>Todoリスト</h1>
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        {{ todo.title }} - {{ todo.completed ? '完了' : '未完了' }}
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
   name: 'App',
+  data() {
+    return {
+      todos: []
+    }
+  },
+  mounted() {
+    this.fetchTodos();
+  },
+  methods: {
+    async fetchTodos() {
+      const response = await axios.get('http://localhost:3000/todos')
+      this.todos = response.data;
+    }
+  }
 };
 </script>
 
